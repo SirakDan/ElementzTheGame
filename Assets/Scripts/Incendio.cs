3a5dd83786b3bb9f1254c9e8973a97a78e901371
NasK1991 <alex_nask91@hotmail.com>
2014-05-27 22:38:01 +0200

    CAMBIO EN INCENDIO.CS
    ahora apaga a las llamas hijas

--- Assets/Scripts/Incendio.cs
+++ Assets/Scripts/Incendio.cs
@@ -2,25 +2,20 @@
 using System.Collections;
 
 public class Incendio : MonoBehaviour {
-
+	
 	public float distanceToExtin = 5f;
 	public float ExtinTime = 0.5f;
-
+	
 	private ParticleSystem chorroAgua;
-
 	private Light FireLight;
 	private ParticleSystem llamas1;
 	private ParticleSystem llamas2;
 	private ParticleSystem llamas3;
 	private float lastExtinTime;
-
+	
 	// Use this for initialization
 	void Start () {
 		chorroAgua = GameObject.Find ("ChorroAgua").GetComponent<ParticleSystem>();
-		//FireLight = GameObject.Find ("FireLight").GetComponent<Light>();
-		//llamas1 = GameObject.Find ("llamas1").GetComponent<ParticleSystem>();
-		//llamas2 = GameObject.Find ("llamas2").GetComponent<ParticleSystem>();
-		//llamas3 = GameObject.Find ("llamas3").GetComponent<ParticleSystem>();
 		FireLight = transform.GetChild (0).GetComponent<Light> ();
 		llamas1 = transform.GetChild (1).GetComponent<ParticleSystem>();
 		llamas2 = transform.GetChild (2).GetComponent<ParticleSystem>();
@@ -38,11 +33,9 @@ public class Incendio : MonoBehaviour {
 				lastExtinTime = Time.time;
 			}
 		}
-		if (llamas1.emissionRate == 0) { 
-			Destroy (gameObject); 
-		}
+		if (llamas1.emissionRate == 0) { Destroy (gameObject); }
 	}
-
+	
 	private bool Apagar() {
 		bool apa = false;
 		GameObject go = GameObject.FindGameObjectWithTag ("Player");
@@ -51,14 +44,15 @@ public class Incendio : MonoBehaviour {
 		float x = transform.position.x - player.transform.position.x;
 		float y = transform.position.y - player.transform.position.y;
 		float h = Mathf.Sqrt(Mathf.Pow(x,2) + Mathf.Pow(y,2));
-
+		
 		bool estaEnLaDerecha = (transform.position.x > player.transform.position.x);
-
-		if (h <= distanceToExtin && chorroAgua.emissionRate > 0 &&
-		    estaEnLaDerecha == player.miraDerecha) { apa = true; }
+		
+		if ( chorroAgua != null) {
+			if (h <= distanceToExtin && chorroAgua.emissionRate > 0 &&
+			    estaEnLaDerecha == player.miraDerecha) { apa = true; }
+		}
 		
 		return apa;
 	}
-
-
+	
 }
